570470fa6781101f5851fcb7dce42db74ca1de99
immanuel3333 <immanuel.siburian80@gmail.com>
2021-11-19 15:33:01 +0700

    submit

---
+++ src/CountContext.js
@@ -0,0 +1,47 @@
+import { createContext, useContext, useReducer } from "react";
+
+const CountContext = createContext();
+const initialState = 0;
+
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case "increment": {
+      return state + 1;
+    }
+    case "decrement": {
+      // code here
+      return state - 1;
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+};
+
+const CountProvider = ({ children }) => {
+  // useReducer
+  const [state, dispatch] = useReducer(countReducer, initialState);
+
+  // Make variable `value` and assign state & dispatch
+  const value = {
+    state: state,
+    dispatch: dispatch,
+  };
+
+  return (
+    <CountContext.Provider value={value}>{children}</CountContext.Provider>
+  );
+};
+
+const useCount = () => {
+  // fill the default value of useContext
+  const context = useContext(CountContext);
+
+  if (context === "undefined") {
+    throw new Error("useCount must be used within a CountProvider");
+  }
+
+  return context;
+};
+
+export { CountProvider, useCount };
